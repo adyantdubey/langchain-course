c014b9579a33b0b1d23f7a8f5eba5a58ecb1a27c
unknown <adyantadubey1234@gmail.com>
2025-12-29 18:34:11 +0530

    react agent implementation first commit

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 from dotenv import load_dotenv
 load_dotenv()
-from langchain.agents import create_agent
-from langchain.tools import tool
-from langchain.messages import HumanMessage
+
+from langchain import hub
+from langchain.agents import AgentExecutor
+from langchain.agents import create_react_agent
 from langchain_openai import ChatOpenAI
-from langchain_ollama import ChatOllama
 from langchain_tavily import TavilySearch
 
-
-
-llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
-# llm = ChatOllama(temperature=0, model="gemma3:270m")
-
-tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+tools  = [TavilySearch()]
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages": [HumanMessage(content="Get me 4 job openings for a person with business experience in tech field near Thane, maharashtra and link their details")]})
-    print(result)
     
 
 if __name__ == "__main__":
