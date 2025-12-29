b1b889b6dcaa92299e89967174c24db08af5a492
unknown <adyantadubey1234@gmail.com>
2025-12-29 08:50:30 +0530

    personal search tool

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain.messages import HumanMessage
 from langchain_openai import ChatOpenAI
+from tavily import TavilyClient
+
+tavily = TavilyClient()
 
 @tool
 def search(query:str) -> str:
@@ -15,15 +18,15 @@ def search(query:str) -> str:
         str: The search results.
     """
     print(f"Searching for {query}...")
-    return "Indian weather is sunny"
+    return tavily.search(query=query)
 
-llm = ChatOpenAI(model="gpt-4", temperature=0)
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 tools = [search]
 agent = create_agent(model=llm, tools=tools)
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages": [HumanMessage(content="What's the weather in India?")]})
+    result = agent.invoke({"messages": [HumanMessage(content="Get me 4 job openings for a person with business experience in tech field near Thane, maharashtra and link their details")]})
     print(result)
     
 
